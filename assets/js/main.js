fb9a0635214f5b895d2bca9dc40f81a120cf3851
dewanhimanshu <himanshudewan786@gmail.com>
2020-09-22 17:37:19 +0530

    added ajax request

--- assets/js/main.js
+++ assets/js/main.js
@@ -191,3 +191,23 @@
   });
 
 })(jQuery);
+
+$("#contact_form").submit(function(e) {
+
+  e.preventDefault(); // avoid to execute the actual submit of the form.
+
+  var form = $(this);
+  var url = form.attr('action');
+  
+  $.ajax({
+         type: "POST",
+         url: url,
+         data: form.serialize(), // serializes the form's elements.
+         success: function(data)
+         {
+             alert(data); // show response from the php script.
+         }
+       });
+
+  
+});
